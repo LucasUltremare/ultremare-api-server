e90243f109e0061527a402b2c69e906103b6b76a
Lucas Ultremare <lucas.ultremare@gmail.com>
2024-11-17 16:21:47 -0300

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Importar APIs dos diferentes módulos
-router.use('/ultremare', require('./ultremare'));
+//router.use('/ultremare', require('./ultremare'));
 //router.use('/projetoX', require('./projetoX'));
 //router.use('/EVA'), require('./EVA')
 // Adicione outras APIs conforme necessário
